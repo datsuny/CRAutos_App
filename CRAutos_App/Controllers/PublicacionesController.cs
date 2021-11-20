e880f7e17d179c18538d14d4cfc384fd05a961ae
johnnyMata <jhonnyjosuemataretana@gmail.com>
2021-11-20 17:32:35 -0600

    se agrego el controlador de publicaciones

---
+++ CRAutos_App/Controllers/PublicacionesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRAutos_App.Controllers
+{
+    public class PublicacionesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
