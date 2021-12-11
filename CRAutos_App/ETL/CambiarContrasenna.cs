acf51366d5a02aad7219ea54af3fda7dd3a09602
EDUARDO ACUÑA <eacuna40450@ufide.ac.cr>
2021-12-11 14:26:49 -0600

    Cambiar Contrasenna datos

---
+++ CRAutos_App/ETL/CambiarContrasenna.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRAutos_App.ETL
+{
+    public class CambiarContrasenna
+    {
+        public string token { get; set; }
+        [Required]
+        public string Contrasenna { get; set; }
+
+        [Compare("Contrasenna")]
+        [Required]
+        public string Contrasenna2 { get; set; }
+    }
+}
